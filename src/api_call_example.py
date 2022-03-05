cc398fd19a7da3e3c592460a688097b8ae1b3dc4
Meeperbunny <mckibbenian@gmail.com>
2022-03-05 15:13:04 -0700

    Added other environ

--- src/api_call_example.py
+++ src/api_call_example.py
@@ -3,3 +3,5 @@ import os
 API_KEY = os.environ.get("TWITTER_API")
 SECRET_KEY = os.environ.get("TWITTER_SECRET")
 BEARER_KEY = os.environ.get("TWITTER_BEARER")
+ACCESS_KEY = os.environ.get("TWITTER_ACCESS")
+ACCESS_SECRET_KEY = os.environ.get("TWITTER_ACCESS_SECRET")
