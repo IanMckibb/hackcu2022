704f592134a138abc20f913e9f5866a03b148e1e
Meeperbunny <mckibbenian@gmail.com>
2022-03-05 18:25:41 -0700

    Rules working

--- src/api_call_examples/api_call_by_hashtag.py
+++ src/api_call_examples/api_call_by_hashtag.py
@@ -1,14 +1,40 @@
-# sampled_stream.py
-
 import json
-from twitter_stream import SampledStream
+from twitter_stream import FilteredStream
 
-class Stream(SampledStream):
+global stream
+
+def delete_rules():
+        curr_rules = stream.get_rules()
+        print(curr_rules)
+        rule_ids = []
+        print("BEFORE: ", curr_rules)
+        if curr_rules['meta']['result_count'] != 0:
+                rule_ids = {
+                        "delete": {
+                                "ids": [n['id'] for n in curr_rules['data']] # example id
+                        }
+                }
+                stream.delete_rule(rule_ids)
+
+class Stream(FilteredStream):
     user_fields = ['name', 'location', 'public_metrics']
-    expansions = ['author_id']
+    expansion = ['author_id']
     tweet_fields = ['created_at']
 
-stream = Stream()
+# Stream to read from
+stream = FilteredStream()
+
+# Rules to modify
+rules: list = [
+    {"value": "(ukrainian OR ukraine) lang:en"}
+]
+
+# Get rid of previous rules
+delete_rules()
+
+# Add current rules
+stream.add_rule(data={"add": rules})
+
+# While connected, print rules
 for tweet in stream.connect():
-#     print(json.dumps(tweet, indent=4))
-        print(tweet['data']['text'])
+    print(json.dumps(tweet, indent=4))
